4c6718f8899ddad90ba653fd4bb73badd3603453
madi-orazbek <madiorazbek546@gmail.com>
2025-07-15 01:30:53 +0500

    Update passport.js

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -6,24 +6,30 @@ const bcrypt = require('bcryptjs');
 module.exports = function(passport) {
   // Стратегия аутентификации
   passport.use(new LocalStrategy(
-    { usernameField: 'username' },
-    async (username, password, done) => {
-      try {
-        const user = await User.findOne({ username });
-        if (!user) {
-          return done(null, false, { message: 'Неверное имя пользователя' });
-        }
-        
-        const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch) {
-          return done(null, false, { message: 'Неверный пароль' });
-        }
-        
-        return done(null, user);
-      } catch (error) {
-        return done(error);
+  { usernameField: 'email' },
+  async (email, password, done) => {
+    try {
+      console.log('Попытка аутентификации:', email);
+      const user = await User.findOne({ email });
+      if (!user) {
+        console.log('Пользователь не найден');
+        return done(null, false, { message: 'User not found' });
       }
+      
+      const isValid = await user.comparePassword(password);
+      if (!isValid) {
+        console.log('Неверный пароль');
+        return done(null, false, { message: 'Incorrect password' });
+      }
+      
+      console.log('Успешная аутентификация:', user.email);
+      return done(null, user);
+    } catch (error) {
+      console.error('Ошибка аутентификации:', error);
+      return done(error);
     }
+  }
+));
   ));
 
   // Сериализация пользователя
