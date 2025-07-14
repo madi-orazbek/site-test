9e1dcb1f9f74cc66142d4dcb9ee5a92fb533bd5f
madi-orazbek <madiorazbek546@gmail.com>
2025-07-15 03:25:44 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -94,6 +94,7 @@ app.use((req, res, next) => {
   res.locals.user = req.user || null;
   res.locals.success = req.flash('success');
   res.locals.error = req.flash('error');
+  res.locals.csrfToken = req.csrfToken();
   console.log('Current user:', res.locals.user ? res.locals.user.email : 'none');
   next();
 });
